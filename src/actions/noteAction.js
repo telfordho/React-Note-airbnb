6286911d6136a80ed7dbd44a4c1d85ee60b988a0
telfordho <telfordho@gmail.com>
2019-01-18 15:46:28 +0800

    first commit

---
+++ src/actions/noteAction.js
@@ -0,0 +1,18 @@
+export const ADD_NOTE_VALUE = 'ADD_NOTE_VALUE';
+export const noteAdd = value => ({
+  type: 'ADD_NOTE_VALUE',
+  value,
+});
+
+export const REMOVE_NOTE_VALUE = 'REMOVE_NOTE_VALUE';
+export const noteRemove = id => ({
+  type: 'REMOVE_NOTE_VALUE',
+  id,
+});
+
+export const REVISE_NOTE_VALUE = 'REVISE_NOTE_VALUE';
+export const noteRevise = (value, id) => ({
+  type: 'REVISE_NOTE_VALUE',
+  value,
+  id,
+});
---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,30 @@
+import { ADD_NOTE_VALUE, REMOVE_NOTE_VALUE, REVISE_NOTE_VALUE } from '../actions/noteAction';
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NOTE_VALUE:
+      return {
+        state: [
+          ...state,
+          action.value,
+        ],
+      };
+    case REMOVE_NOTE_VALUE:
+      return {
+        state: state.filter(note => note.id !== action.id),
+      };
+    case REVISE_NOTE_VALUE:
+      return {
+        state: state.map((note) => {
+          if (note.id === action.id) {
+            return action.value;
+          }
+          return note;
+        }),
+      };
+    default:
+      return state;
+  }
+};
